7356b2f6ec2d14c124e00f4dc76e2f99934a275b
Kathleen <mullins.k@husky.neu.edu>
2014-11-08 22:05:56 -0500

    basic done

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('form.jqtransform').jqTransform();
+	$('.meridian span').on('click', function(e){
+		$('.meridian .am').toggleClass('selected');
+		$('.meridian .pm').toggleClass('selected');
+	});
+	$('#add-notification').on('click', function(e){
+		$('.reminder-form').show();
+
+	});
+});
